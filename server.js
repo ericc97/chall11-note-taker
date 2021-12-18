1bbb9ae2edea6cf6c10e4860bde113a3c2ba8504
Eric Coleman <ericcoleman9217@gmail.com>
2021-12-18 17:09:52 -0600

    set up basic express functions

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const fs = require('fs');
+
+
 const path = require('path');
 const app = express()
 const port = 3000
-// request
+const notes = require('./Develop/db/notes.json')
 
+//require middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
+
+app.get('/api/notes', (req,res) => {
+   res.json({notes}) 
+})
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
