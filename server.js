e6c1c9683adf40d37b96f68ba53aae00eb5365ae
Eric Coleman <ericcoleman9217@gmail.com>
2021-12-18 12:57:07 -0600

    added node modules to gitignore file

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+// request
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// request -> middleware function -> response
+
+// server side rendered application
+
+// react -> client side rendered
